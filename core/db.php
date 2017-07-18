4faf619dff18cce257ca60c76970f07dd47107df
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-07-19 00:08:50 +0200

    Update db.php

---
+++ core/db.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = $GLOBALS['config']['mysql'];
+try{
+
+  $db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['db'] ,
+    $config['username']);
+
+} catch(Exception $e){
+  echo error('DB Error: ' . $e->getMessage() );
+}
