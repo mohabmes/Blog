7cb58fbe93c82ecc74f4782bd57ab0ad21556de4
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-07-21 03:20:09 +0200

    Create header.php

---
+++ app/views/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title><?php echo $header; ?></title>
+		<meta charset="utf-8" lang="en">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<!--link rel="icon" type="image/icon" href=""-->
+		<link type="text/css" rel="stylesheet" href="https://necolas.github.io/normalize.css/latest/normalize.css">
+		<link type="text/css" rel="stylesheet" href="<?php echo CSS.'style.css'; ?>">
+	</head>
+	<body>
+		<nav>
+			<div class="wrapper">
+				<ul>
+					<li><a href="<?php echo BASE_URL; ?>">Home</a></li>
+					<li><a href="<?php echo BASE_URL . '/posts.php'; ?>">All Posts</a></li>
+				</ul>
+			</div>
+	  </nav>
