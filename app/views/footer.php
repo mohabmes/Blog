d9904d7d7c8767a8b34e44a51ce63e0d6fe69ae3
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-07-21 03:18:07 +0200

    Update footer.php

---
+++ app/views/footer.php
@@ -0,0 +1,12 @@
+<footer>
+  <div class="wrapper">
+    <ul>
+      <li><a href="#">Github</a></li>
+      <li><a href="#">Linkedin</a></li>
+      <li><a href="#">Facebook</a></li>
+    </ul>
+    <p>&copy;2017 All Rights Reserved.</a></p>
+  </div>
+</footer>
+</body>
+</html>
