5dcd429152febeda9099241545890a15aad78d11
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-07-24 00:06:40 +0200

    Update header.php

--- app/views/header.php
+++ app/views/header.php
@@ -6,6 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<!--link rel="icon" type="image/icon" href=""-->
+		<base href="//localhost/Blog/" />
 		<link type="text/css" rel="stylesheet" href="https://necolas.github.io/normalize.css/latest/normalize.css">
 		<link type="text/css" rel="stylesheet" href="<?php echo CSS.'style.css'; ?>">
 	</head>
@@ -14,7 +15,7 @@
 			<div class="wrapper">
 				<ul>
 					<li><a href="<?php echo BASE_URL; ?>">Home</a></li>
-					<li><a href="<?php echo BASE_URL . '/posts.php'; ?>">All Posts</a></li>
+					<li class="separate"><a href="<?php echo BASE_URL . '/posts/'; ?>">All Posts</a></li>
 				</ul>
 			</div>
 	  </nav>
