eb28b43d518a016589c5b2be614928b9bd3a3ac9
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-07-23 03:23:28 +0200

    Add more functionality to Blog class

--- app/classes/Blog.php
+++ app/classes/Blog.php
@@ -6,23 +6,28 @@ class Blog {
           $_tags,
           $_created,
           $_updated = null,
-          $_error = array();
+          $_error = array(),
+          $_db;
 
-  // function __construct(){
-  //   echo "string";
-  // }
+  function __construct(){
+    global $db;
+    $this->_db = $db;
+  }
 
   public function create($blog = array()) {
-    global $db;
+
     if(isset($blog) && !empty($blog) && sizeof($blog)==4) {
       $this->_title = trim(filter_var($blog['title'], FILTER_SANITIZE_STRING));
       $this->_body =  filter_var($blog['body'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-      $this->_slug =  filter_var($blog['slug'], FILTER_SANITIZE_STRING);
+      $this->_slug =  $this->slugFilter(filter_var($blog['slug'], FILTER_SANITIZE_STRING));
       $this->_tags  = filter_var($blog['tags'], FILTER_SANITIZE_STRING);
       $this->_created = date('F, j Y');
 
+    if(!Tags::exists($this->_tags)){
+      Tags::create($this->_tags);
+    }
     if($this->validate()){
-        $qry = $db->prepare('INSERT INTO posts (`slug`, `title`, `body`,  `created`,  `tags`) VALUES ( :slug, :title, :body, :created, :tags)');
+        $qry = $this->_db->prepare('INSERT INTO posts (`slug`, `title`, `body`,  `created`,  `tags`) VALUES ( :slug, :title, :body, :created, :tags)');
         $qry->execute([
           'slug' => $this->_slug,
           'title' => $this->_title,
@@ -46,9 +51,12 @@ class Blog {
         $this->_tags  = filter_var($blog['tags'], FILTER_SANITIZE_STRING);
         $this->_updated = date('F, j Y');
 
+        if(Tags::exists($this->_tags)){
+          Tags::create($this->_tags);
+        }
         if($this->validate()){
           $sql = "UPDATE `posts` SET `slug`= :slug,`title`= :title,`body`= :body,`tags`= :tags,`updated`= :updated WHERE `id`= :id";
-          $qry = $db->prepare($sql);
+          $qry = $this->_db->prepare($sql);
           $qry->execute([
             'id' => $id,
             'slug' => $this->_slug,
@@ -66,7 +74,7 @@ class Blog {
 
   public function delete($id){
     global $db;
-    $qry = $db->prepare('DELETE FROM `posts` WHERE id = :id');
+    $qry = $this->_db->prepare('DELETE FROM `posts` WHERE id = :id');
     $qry->execute([
       'id' => $id
     ]);
@@ -75,7 +83,7 @@ class Blog {
 
   public function getById($id){
     global $db;
-    $qry = $db->prepare('SELECT * FROM `posts` WHERE id = :id');
+    $qry = $this->_db->prepare('SELECT * FROM `posts` WHERE id = :id');
     $qry->execute([
       'id' => $id
     ]);
@@ -83,39 +91,82 @@ class Blog {
     return $result;
   }
 
-    private function checkSlug($slug){
-      global $db;
-      $qry = $db->prepare('SELECT COUNT(id) FROM posts WHERE slug = :slug');
-      $qry->execute([
-        'slug' => $slug
-      ]);
-      $result = $qry->fetch(PDO::FETCH_NUM);
-      return $result[0]>0 ? false : true;
-    }
+  public function getFrom($start, $count){
+    global $db;
+    $qry = $this->_db->prepare("SELECT * FROM `posts` LIMIT {$start}, {$count}");
+    $qry->execute();
+    $result = $qry->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+  }
 
-    public function validate() {
-        if($this->checkSlug($this->_slug) && strlen($this->_slug)<30 && strlen($this->_slug)>=10){
-          if(strlen($this->_title)<50 && strlen($this->_title)>=20) {
-            if(!empty($this->_tags)){
-              if(strlen($this->_body)>=200) {
-                return true;
-              } else {
-                $this->_error[] = 'Blog must more than 200';
-              }
+  public function getRecent($count){
+    global $db;
+    $qry = $this->_db->prepare("SELECT * FROM `posts` ORDER BY `id` DESC LIMIT {$count}");
+    $qry->execute();
+    $result = $qry->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+  }
+
+  public function getDate($created, $updated){
+    if(!empty($updated)){
+      return "Created {$created} - Updated {$updated}";
+    } else {
+      return "Created {$created}";
+    }
+  }
+
+  public function getSlug($slug){
+    $url =  BASE_URL . '/post/' . $slug;
+    return $url;
+  }
+
+  public function getBody($body){
+    return nl2br($body);
+  }
+
+  public function getPreviewBody($body){
+    $body = substr($body, 450);
+    return $body . '...';
+  }
+
+  private function checkSlug($slug){
+    global $db;
+    $qry = $this->_db->prepare('SELECT COUNT(id) FROM posts WHERE slug = :slug');
+    $qry->execute([
+      'slug' => $slug
+    ]);
+    $result = $qry->fetch(PDO::FETCH_NUM);
+    return $result[0]>0 ? false : true;
+  }
+
+  private function validate() {
+      if($this->checkSlug($this->_slug) && strlen($this->_slug)<30 && strlen($this->_slug)>=10){
+        if(strlen($this->_title)<50 && strlen($this->_title)>=20) {
+          if(!empty($this->_tags)){
+            if(strlen($this->_body)>=200) {
+              return true;
             } else {
-              $this->_error[] = 'Must provide tag';
+              $this->_error[] = 'Blog must more than 200';
             }
           } else {
-            $this->_error[] = 'Title must be more than 20 and less than 50';
+            $this->_error[] = 'Must provide tag';
           }
         } else {
-          $this->_error[] = 'Slug must be unique, more than 10 and less than 25';
+          $this->_error[] = 'Title must be more than 20 and less than 50';
         }
-      return false;
-    }
+      } else {
+        $this->_error[] = 'Slug must be unique, more than 10 and less than 25';
+      }
+    return false;
+  }
 
-    public function error() {
-      return $this->_error;
-    }
+  private function slugFilter($slug){
+     $slug = str_replace(' ', '-', $slug);
+     return $slug;
+  }
+
+  public function error() {
+    return $this->_error;
+  }
 
 }
