fa75100bdec207d75912ffcc223ca00302e4007a
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-07-23 03:19:49 +0200

    Update ini.php

--- core/ini.php
+++ core/ini.php
@@ -1,14 +1,20 @@
 <?php
 session_start();
 
-//ini_set('display_errors', 'On');
+ini_set('display_errors', 'On');
 
-define('CORE' ,    __DIR__.'/');
-define('APP'  ,   './app/');
+define('CORE',    __DIR__.'/');
+define('APP',     './app/');
+define('classes', './app/classes/');
 define('ADMIN',   './admin/');
-define('CSS'  ,   './resources/css/');
-define('BASE_URL', 'http://localhost/Blog/');
+define('CSS',     './resources/css/');
+define('BASE_URL','http://localhost/Blog');
 
 require_once(CORE . 'function.php');
 require_once(CORE . 'config.php');
 require_once(CORE . 'db.php');
+
+
+require_once(classes . 'Tags.php');
+require_once(classes . 'Blog.php');
+require_once(classes . 'Input.php');
