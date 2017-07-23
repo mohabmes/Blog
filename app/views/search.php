6779124e01ba1a860738b2bdac6790918d126fd2
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-07-24 00:11:34 +0200

    Create search Template

---
+++ app/views/search.php
@@ -0,0 +1,45 @@
+<?php
+  $header = 'MMES | All posts';
+  require_once APP . 'views/header.php';
+?>
+<div class="wrapper">
+    <form action="search/" method="get" class="form">
+      <center>
+        <input type="text" name="search" placeholder="Search" value="<?php echo Input::get('search'); ?>">
+        <input type="submit" value="Search">
+      </center>
+    </form>
+
+  <div class="panel">
+    <p class="title">Results for " <?php echo $search; ?> "</p>
+      <?php
+        if(isset($result) && !empty($result)){
+          foreach ($result as $post){
+            $post['created'] = $blog->getDate($post['created'], $post['updated'] );
+            $post['slug'] = $blog->getSlug($post['slug']);
+            $post['body'] = $blog->getPreviewBody($post['body']);
+            echo postPreview($post);
+          }
+        } else {
+          echo center('<< No result found >>');
+        }
+      ?>
+  </div>
+
+  <div class="tags">
+    <p class="title">Tags</p>
+    <?php
+      if(isset($tags) && !empty($tags)){
+        foreach ($tags as $tag ){
+          echo tagsPreview($tag['title']);
+        }
+      } else {
+        echo center('<< No Tag yet >>');
+      }
+    ?>
+  </div>
+  <br>
+  <div class="clearfix"></div>
+</div>
+<?php
+  require_once APP . 'views/footer.php';
