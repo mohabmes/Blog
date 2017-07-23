96e3d3087f31d31b5f83d5e1b3f3e7dcf52bd3bf
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-07-23 03:18:40 +0200

    Create class for tags

---
+++ app/classes/Tags.php
@@ -0,0 +1,30 @@
+<?php
+class Tags{
+  public static function get(){
+      global $db;
+      $qry = $db->prepare('SELECT title FROM tags');
+      $qry->execute();
+      $result = $qry->fetchAll(PDO::FETCH_ASSOC);
+      return $result;
+  }
+
+  public static function exists($tag){
+      global $db;
+      $tag  = filter_var($tag, FILTER_SANITIZE_STRING);
+      $qry = $db->prepare('SELECT COUNT(id) FROM tags WHERE title = :title');
+      $qry->execute([
+        'title' => $tag
+      ]);
+      $result = $qry->fetch(PDO::FETCH_NUM);
+      return $result[0] > 0 ? true : false;
+  }
+
+  public static function create($tag){
+      global $db;
+      $tag  = filter_var($tag, FILTER_SANITIZE_STRING);
+      $qry = $db->prepare('INSERT INTO tags (`title`) VALUES ( :title)');
+      $qry->execute([
+        'title' => $tag
+      ]);
+  }
+}
