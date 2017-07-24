ef108f765e7cc65e9ccaed9f9af26e206b9d5577
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-07-25 00:59:57 +0200

    Update function.php

--- core/function.php
+++ core/function.php
@@ -1,6 +1,6 @@
 <?php
 function upper($str){
-  return strtoupper($str);
+  return ucfirst($str);
 }
 
 function error($str){
@@ -8,21 +8,41 @@ function error($str){
 }
 
 function center($str){
-  return '<br><center>'. upper($str) .'</center>';
+  return '<br><center>'. $str .'</center>';
 }
 
 function postPreview($blog){
   return
-  "<a href='{$blog['slug']}'>
-    <div class='post'>
-      <p class='post-title'>{$blog['title']}</p>
+  "<div class='post'>
+      <a href='{$blog['slug']}'>
+        <p class='post-title'>{$blog['title']}</p>
+      </a>
       <p class='post-date'>{$blog['created']}</p>
       <p class='post-body'>{$blog['body']}</p>
-    </div>
-  </a>";
+  </div>";
+}
+
+function commentPreview($comment){
+  return
+  "<div class='comment'>
+    <b>{$comment['name']}</b>
+    <span class='separate'> | </span>
+    <i>{$comment['created']}</i>
+    <p>{$comment['text']}
+    </p>
+  </div>";
 }
 
 function tagsPreview($tag){
   $url = BASE_URL . '/search/?tag=' . $tag;
   return "<a href='{$url}'><span>{$tag}</span></a>";
+}
+
+function pages($i){
+  $url = BASE_URL . '/posts/' . $i;
+  return "<li><a href='{$url}'>{$i}</a></li>";
+}
+
+function escape($str){
+  return htmlentities($str, ENT_QUOTES, "UTF-8");
 }
