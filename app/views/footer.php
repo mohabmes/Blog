ef3797e80fc1249794d94d96d07dda5abde3af69
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-08-10 22:03:31 +0200

    Fix Hyperlink

--- app/views/footer.php
+++ app/views/footer.php
@@ -2,7 +2,7 @@
   <div class="wrapper">
     <ul>
       <li><a href="https://github.com/mohabmes">Github</a></li>
-      <li><a href="https://www.linkedin.com/m/mohabmes">Linkedin</a></li>
+      <li><a href="https://www.linkedin.com/in/mohabmes">Linkedin</a></li>
       <li><a href="https://www.facebook.com/mohabmes">Facebook</a></li>
     </ul>
     <p>&copy;2017 All Rights Reserved.</a></p>
